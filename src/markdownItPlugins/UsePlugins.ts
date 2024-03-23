0880508f149c69b7e461ef13d43b885f60c8f52f
nmmgk <tatuu1306@gmail.com>
2024-03-23 20:31:21 +0900

    add dakuten

--- src/markdownItPlugins/UsePlugins.ts
+++ src/markdownItPlugins/UsePlugins.ts
@@ -3,6 +3,7 @@ import MarkdownItContainer from "markdown-it-container";
 import Token from "markdown-it/lib/token";
 import { ImageRule } from "./ImageRule";
 import { ParseFrontMatter } from "./ParseFrontMatter";
+import StateCore from "markdown-it/lib/rules_core/state_core";
 
 export const UsePlugins = (md: MarkdownIt) => {
   md.use(ParseFrontMatter);
@@ -35,4 +36,13 @@ export const UsePlugins = (md: MarkdownIt) => {
 
   // image
   md.use(ImageRule);
+
+  md.renderer.rules.text = (tokens: Token[], idx: number) => {
+    const token = tokens[idx];
+    const regex = /(.)(゛)/g;
+    const replaced = token.content.replace(regex, (_match, p1) => {
+      return `<span class="dakuten">${p1}</span>`;
+    });
+    return replaced;
+  };
 };
